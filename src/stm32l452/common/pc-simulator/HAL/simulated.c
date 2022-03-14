f32b15890f97b20f10574a54703534e53bc34ef3
Solartraveler <Solartraveler@users.noreply.github.com>
2022-03-14 23:06:16 +0100

    pc-simulator: Add replacements for enable and disable interrupts.

--- src/stm32l452/common/pc-simulator/HAL/simulated.c
+++ src/stm32l452/common/pc-simulator/HAL/simulated.c
@@ -8,6 +8,7 @@ SPDX-License-Identifier: BSD-3-Clause
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "simulated.h"
 
@@ -16,12 +17,30 @@ SPDX-License-Identifier: BSD-3-Clause
 
 const uint8_t g_dummyLoader[ROM_LOADER_SIZE];
 
+pthread_mutex_t g_IsrLocked;
+
+void SimulatedInit(void) {
+	pthread_mutex_init(&g_IsrLocked, NULL);
+}
+
+void SimulatedDeinit(void) {
+	pthread_mutex_destroy(&g_IsrLocked);
+}
 
 void NVIC_SystemReset(void) {
 	printf("%s called\n", __func__);
+	SimulatedDeinit();
 	exit(0);
 }
 
 void HAL_Delay(uint32_t delay) {
 	usleep(delay * 1000);
+}
+
+void __disable_irq(void) {
+	pthread_mutex_lock(&g_IsrLocked);
+}
+
+void __enable_irq(void) {
+	pthread_mutex_unlock(&g_IsrLocked);
 }
--- src/stm32l452/common/pc-simulator/HAL/simulated.h
+++ src/stm32l452/common/pc-simulator/HAL/simulated.h
@@ -2,10 +2,21 @@
 
 #include <stdint.h>
 
+//Call before using any other function:
+void SimulatedInit(void);
+
+//Call before exit, to clean things up
+void SimulatedDeinit(void);
+
 void NVIC_SystemReset(void);
 
 void HAL_Delay(uint32_t delay);
 
 extern const uint8_t g_dummyLoader[];
+
+//Not really disabling IRSs, its just a common lock.
+void __disable_irq(void);
+
+void __enable_irq(void);
 
 #define ROM_BOOTLOADER_START_ADDRESS ((uintptr_t)(&g_dummyLoader))
