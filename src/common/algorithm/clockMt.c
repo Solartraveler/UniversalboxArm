7f614f02aa1b54d2b3c7a56672af43696326c74f
Solartraveler <Solartraveler@users.noreply.github.com>
2022-12-04 14:23:49 +0100

    Common: Algorithm: Add thread safe access to clock
    Mt suffix will be used for multi threading safe functions.

---
+++ src/common/algorithm/clockMt.c
@@ -0,0 +1,77 @@
+/* Boxlib
+(c) 2022 by Malte Marwedel
+
+SPDX-License-Identifier:  BSD-3-Clause
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "clockMt.h"
+
+#include "boxlib/clock.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+#define MUTEX_TIMEOUT 100
+
+SemaphoreHandle_t g_clockSemaphore;
+StaticSemaphore_t g_clockSemaphoreState;
+
+
+void ClockPrintRegistersMt(void) {
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		ClockPrintRegisters();
+		xSemaphoreGive(g_clockSemaphore);
+	}
+}
+
+bool ClockInitMt(void) {
+	g_clockSemaphore = xSemaphoreCreateMutexStatic(&g_clockSemaphoreState);
+	bool result = ClockInit();
+	xSemaphoreGive(g_clockSemaphore);
+	return result;
+}
+
+void ClockDeinitMt(void) {
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		ClockDeinit();
+		//do not give back the mutex. Another call to init will overwrite the state.
+	}
+}
+
+uint32_t ClockUtcGetMt(uint16_t * pMiliseconds) {
+	uint32_t result = 0;
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		result = ClockUtcGet(pMiliseconds);
+		xSemaphoreGive(g_clockSemaphore);
+	}
+	return result;
+}
+
+uint8_t ClockSetTimestampGetMt(uint32_t * pUtc, uint16_t * pMiliseconds) {
+	uint8_t result = 0;
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		result = ClockSetTimestampGet(pUtc, pMiliseconds);
+		xSemaphoreGive(g_clockSemaphore);
+	}
+	return result;
+}
+
+bool ClockUtcSetMt(uint32_t timestamp, uint16_t miliseconds, bool precise, int64_t * pDelta) {
+	bool result = false;
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		result = ClockUtcSet(timestamp, miliseconds, precise, pDelta);
+		xSemaphoreGive(g_clockSemaphore);
+	}
+	return result;
+}
+
+int32_t ClockCalibrationGetMt(void) {
+	int32_t result = 0;
+	if (xSemaphoreTake(g_clockSemaphore, MUTEX_TIMEOUT)) {
+		result = ClockCalibrationGet();
+		xSemaphoreGive(g_clockSemaphore);
+	}
+	return result;
+}
---
+++ src/common/algorithm/clockMt.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//Gives access to the RTC clock, starts the external LSE, sets up the clock source.
+//Returns true on success
+bool ClockInitMt(void);
+
+//Resets the clock, looses the calibration and stops the LSE.
+void ClockDeinitMt(void);
+
+//Gets the unix UTC timestamp. Unit is [s] since 1.1.1970.
+//Should work from 2000 up to the year 2100.
+//pMiliseconds is optional
+uint32_t ClockUtcGetMt(uint16_t * pMiliseconds);
+
+/*
+Returns the value of the last ClockUtcSet timestamp.
+Return value: 0 = No value was ever set, written values might be random
+              1 = Value returned is imprecise
+              2 = Value returned is precise
+*/
+uint8_t ClockSetTimestampGetMt(uint32_t * pUtc, uint16_t * pMiliseconds);
+
+
+/*Sets the unix UTC timestamp. Unit of timestamp is [s] since 1.1.1970.
+  Should work from 2000 up to the year 2100.
+  miliseconds allows a the time to set more precise.
+  The timestamp is stored in the first two backup registers too.
+  An automatic calibration of the clock is done, if all of the following conditions are met:
+  1. Precision must be set to true
+  2. The previous call must have been precision to be true too.
+  3. The previous call must have been between 8h and 24 days in the past.
+  4. The resulting delta must fit within the calibration range +-488ppm
+  pDelta: If not null, the delta in [ms] between the new and old time is written to the variable.
+  Returns true on success
+*/
+bool ClockUtcSetMt(uint32_t timestamp, uint16_t miliseconds, bool precise, int64_t * pDelta);
+
+/* Returns the current calibration value in ppb (parts per billion)
+*/
+int32_t ClockCalibrationGetMt(void);
+
+//For debugging the clock
+void ClockPrintRegistersMt(void);
