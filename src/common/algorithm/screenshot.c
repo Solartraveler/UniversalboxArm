6834011c4585997cf6e5e3d4405ff99bc0950bcc
Solartraveler <Solartraveler@users.noreply.github.com>
2023-07-29 16:54:33 +0200

    Common: Algorithm: Add another option for taking screenshots
    The new function allows using a custom palette and scale the screen.

--- src/common/algorithm/screenshot.c
+++ src/common/algorithm/screenshot.c
@@ -51,3 +51,41 @@ bool Screenshot(void) {
 	printf("Saved %s with %s\r\n", filename, success ? "success" : "failure");
 	return success;
 }
+
+bool ScreenshotPalette24(uint8_t scale, const uint32_t * colormap, const uint8_t colors) {
+	printf("Saving screnshot...\r\n");
+	bool success = true;
+	fileBuffer_t fb;
+	char filename[64];
+	FB_SCREENPOS_TYPE pixelX, pixelY;
+	menu_screen_size_get(&pixelX, &pixelY);
+	//allocate output buffer and open file
+	f_mkdir("/screenshots");
+	uint32_t unusedId = FilesystemGetUnusedFilename("/screenshots", "shot");
+	snprintf(filename, sizeof(filename), "/screenshots/shot%04u.tga", (unsigned int)unusedId);
+	if ((unusedId == 0) || (!FilesystemBufferwriterStart(&fb, filename))) {
+		printf("Error, could not write file %s\r\n", filename);
+		return false;
+	}
+	//write header
+	success &= ImgTgaStart(pixelX * scale, pixelY * scale, colors, 24, true, &FilesystemBufferwriterAppend, &fb);
+	success &= ImgTgaAppend32To24(colormap, colors, &FilesystemBufferwriterAppend, &fb);
+	//write image data
+	for (uint32_t y = 0; y < pixelY; y++) {
+		uint8_t data[pixelX * scale];
+		for (uint32_t x = 0; x < pixelX; x++) {
+			data[x * scale] = menu_screen_get(x, y);
+			for (uint32_t s = 1; s < scale; s++) {
+				data[x * scale + s] = data[x * scale];
+			}
+		}
+		for (uint32_t s = 0; s < scale; s++) {
+			success &= ImgTgaAppendCompress1Byte(data, pixelX * scale, &FilesystemBufferwriterAppend, &fb);
+		}
+		//success &= ImgTgaAppendDirect(data, pixelX, &FilesystemBufferwriterAppend, &fb);
+	}
+	//close file
+	success &= FilesystemBufferwriterClose(&fb);
+	printf("Saved %s with %s\r\n", filename, success ? "success" : "failure");
+	return success;
+}
--- src/common/algorithm/screenshot.h
+++ src/common/algorithm/screenshot.h
@@ -3,3 +3,6 @@
 #include <stdbool.h>
 
 bool Screenshot(void);
+
+bool ScreenshotPalette24(uint8_t scale, const uint32_t * colormap, const uint8_t colors);
+
