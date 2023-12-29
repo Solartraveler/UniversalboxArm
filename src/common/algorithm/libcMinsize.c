dba37e42acd4dd493cb93c3016aef0d5abeb90f1
Solartraveler <Solartraveler@users.noreply.github.com>
2023-12-29 16:26:05 +0100

    Common: Algorithm: Fix programs not working with GCC 12
    The new compiler replaced the size optimized strlen implementation
    with a call to strlen, ending in an endless loop.
    This made all programs which compiled libcMinsize.c to be not working
    anylonger when switching from Debian 11 to Debian 12 (which delivers GCC 12).

--- src/common/algorithm/libcMinsize.c
+++ src/common/algorithm/libcMinsize.c
@@ -7,6 +7,13 @@
 
 #include "femtoVsnprintf.h"
 
+/*
+Thanks goes to https://www.mikrocontroller.net/topic/562568?goto=7562337#7562337
+See bug report: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=113049#c6
+*/
+#pragma GCC push_options
+#pragma GCC optimize ("-fno-tree-loop-distribute-patterns")
+
 size_t strlen(const char * text) {
 	size_t len = 0;
 	while(*text) {
@@ -28,3 +35,5 @@ int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
 	femtoVsnprintf(str, size, format, ap);
 	return strlen(str);
 }
+
+#pragma GCC pop_options
