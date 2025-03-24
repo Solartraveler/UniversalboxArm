b993576c0d3210fd47a2fab9f74ca00325cf1971
Solartraveler <Solartraveler@users.noreply.github.com>
2025-03-25 00:19:03 +0100

    WIP: 15-mp3-player: Play a mp3 file over 8 bit PWM output
    State:
    Playback on the PC works
    Playback on the ARM works but sometimes needs more output buffer
    to not have a break during browsing through the files at the same time.

---
+++ src/apps/15-mp3-player/gui.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "boxlib/lcd.h"
+
+//needs the filesystem running, to get the display type
+void GuiInit(void);
+
+//call periodically to react to key presses
+//key can be from the serial input. 0 for doing nothing.
+void GuiCycle(char key);
